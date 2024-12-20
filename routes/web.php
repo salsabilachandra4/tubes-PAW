fdd5cd7d4cb52c1e64e1c8bc79fcd0d2871157b9
salsabilachandra4 <salsabilachandraa@gmail.com>
2024-12-20 20:59:05 +0700

    tambah tampilan

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/home', function () {
+    return view('pages.home');
+});
+
+Route::get('/home/detail', function () {
+    return view('pages.detail');
+});
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+});
